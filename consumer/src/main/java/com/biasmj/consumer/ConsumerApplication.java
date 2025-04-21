4ba84a21d09320310c6572697199b3681157177a
biasmj016 <mjkim031@gmail.com>
2025-04-21 23:23:25 +0900

    consumer setting

--- consumer/src/main/java/com/biasmj/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/biasmj/consumer/ConsumerApplication.java
@@ -2,6 +2,9 @@ package com.biasmj.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class ConsumerApplication {
@@ -10,4 +13,8 @@ public class ConsumerApplication {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
+    @Bean
+    public Consumer<String> input() {
+        return message -> System.out.println("[Received] " + message);
+    }
 }
